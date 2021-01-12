70eb11a1c01ce2cc030e43310f8c47b5bdbbf33d
huault.christophe <huault.christophe@gmail.com>
2021-01-12 16:37:46 +0100

    Rich text view

--- src/app/content/rich-text/rich-text.component.ts
+++ src/app/content/rich-text/rich-text.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {GraphqlService} from "../../../services/graphql.service";
 
+import { environment} from "../../../environments/environment";
+
 @Component({
   selector: 'app-rich-text',
   templateUrl: './rich-text.component.html',
@@ -23,6 +25,16 @@ export class RichTextComponent implements OnInit {
         console.log("Error " + error);
       }
     );
+  }
+
+  getText(){
+    let toReturn: string = this.bigText.text.value;
+
+    console.log("DEBUG " + toReturn);
+
+    toReturn = toReturn.replace(/\/files\/\{workspace\}/gm, environment.jahiaHost + "/files/live")
+
+    return toReturn;
   }
 
 }

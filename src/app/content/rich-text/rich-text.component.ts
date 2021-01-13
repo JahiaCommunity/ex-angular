65d2793e2a1bbfc6672451c715c26e7002402526
huault.christophe <huault.christophe@gmail.com>
2021-01-13 11:50:52 +0100

    Implement vues

--- src/app/content/rich-text/rich-text.component.ts
+++ src/app/content/rich-text/rich-text.component.ts
@@ -31,7 +31,8 @@ export class RichTextComponent implements OnInit {
   getText(){
     if (this.bigText != null){
       let toReturn: string = this.bigText.text.value;
-      toReturn = toReturn.replace(/\/files\/\{workspace\}/gm, environment.jahiaHost + "/files/live")
+      toReturn = toReturn.replace(/\/files\/\{workspace\}/gm, environment.jahiaHost + "/files/live");
+      toReturn = toReturn.replace(/\/cms\/\{mode\}\/\{lang\}/gm, environment.jahiaHost + "/cms/live/en");
       return toReturn;
     } else {
       return "----";
